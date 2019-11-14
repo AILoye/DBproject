2b65b5644c7835755deaddaca4fa7378af80da4a
BK1918weiqianqian <2628556303@qq.com>
2019-11-14 20:16:34 +0800

    优化

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -9,12 +9,4 @@ let componentMap=[
 componentMap.forEach(item=>{
     Vue.component(item.name,item);
 })
-// import Vue from "vue"
-// import BScroll from "./scroll"
 
-// let componentMap = {
-//     BScroll
-// }
-// componentMap.forEach(item => {
-//     Vue.component(item.name,item)
-// });
